ca29adde70abdcffc6f06cd295815754c270a80c
bepsoccer <brad@parker1723.com>
2017-04-25 21:13:00 -0400

    just fixing spacing

--- route53/editRecord.tcl
+++ route53/editRecord.tcl
@@ -8,25 +8,25 @@ when HTTP_REQUEST {
             set apmProfileName [ACCESS::session data get session.access.profile]
             set apmPart [ACCESS::session data get session.partition_id]
             set DNSName "<your zone with trailing dot>"
-	        set action "UPSERT"
-	        set name "<your hostname>"
-	        if {not ($name ends_with ".[string trimright ${DNSName} {.}]")}{
-	            log -noname local1.err "${apmProfileName}:${apmPart}:${apmSessionId}: proper hostname was not provided ($name): 01490000: A Record will not be updated for this session."
-	            event disable all
-	            return
-	        }
-	        set TTL 1200
-	        set ip [ACCESS::session data get "session.assigned.clientip"]
-	        if {[catch {IP::addr $ip mask 255.255.255.255}]}{
-	            log -noname local1.err "${apmProfileName}:${apmPart}:${apmSessionId}: proper IP addressed was not assigned ($ip): 01490000: A Record will not be updated for this session."
-	            event disable all
-	            return
-	        }
-	        set RPC_HANDLE [ILX::init route53] 
-	        if {[catch {set rpc_response [ILX::call $RPC_HANDLE route53_nodejs $DNSName $action $name $TTL $ip]}]}{
-	            set rpc_response "Check LTM log for execution errors from sdmd.  A Record may not have been updated for this session."
-	        }
-	        log -noname local1. "${apmProfileName}:${apmPart}:${apmSessionId}: Client connected: 01490000: Params sent = $DNSName $action $name $TTL $ip\r\nRPC response = $rpc_response"
+            set action "UPSERT"
+            set name "<your hostname>"
+            if {not ($name ends_with ".[string trimright ${DNSName} {.}]")}{
+                log -noname local1.err "${apmProfileName}:${apmPart}:${apmSessionId}: proper hostname was not provided ($name): 01490000: A Record will not be updated for this session."
+                event disable all
+                return
+            }
+            set TTL 1200
+            set ip [ACCESS::session data get "session.assigned.clientip"]
+            if {[catch {IP::addr $ip mask 255.255.255.255}]}{
+                log -noname local1.err "${apmProfileName}:${apmPart}:${apmSessionId}: proper IP addressed was not assigned ($ip): 01490000: A Record will not be updated for this session."
+                event disable all
+                return
+            }
+            set RPC_HANDLE [ILX::init route53]
+            if {[catch {set rpc_response [ILX::call $RPC_HANDLE route53_nodejs $DNSName $action $name $TTL $ip]}]}{
+                set rpc_response "Check LTM log for execution errors from sdmd.  A Record may not have been updated for this session."
+            }
+            log -noname local1. "${apmProfileName}:${apmPart}:${apmSessionId}: Client connected: 01490000: Params sent = $DNSName $action $name $TTL $ip\r\nRPC response = $rpc_response"
         }
     }
 }
@@ -49,9 +49,9 @@ when ACCESS_SESSION_CLOSED {
         event disable all
         return
     }
-	set RPC_HANDLE [ILX::init route53] 
-	if {[catch {set rpc_response [ILX::call $RPC_HANDLE route53_nodejs $DNSName $action $name $TTL $ip]}]}{
-	    set rpc_response "Check LTM log for execution errors from sdmd.  A Record may not have been removed for this session."
-	}
-	log -noname local1. "${apmProfileName}:${apmPart}:${apmSessionId}: Client disconnected: 01490000: Params sent = $DNSName $action $name $TTL $ip\r\nRPC response = $rpc_response"
+    set RPC_HANDLE [ILX::init route53]
+    if {[catch {set rpc_response [ILX::call $RPC_HANDLE route53_nodejs $DNSName $action $name $TTL $ip]}]}{
+        set rpc_response "Check LTM log for execution errors from sdmd.  A Record may not have been removed for this session."
+    }
+    log -noname local1. "${apmProfileName}:${apmPart}:${apmSessionId}: Client disconnected: 01490000: Params sent = $DNSName $action $name $TTL $ip\r\nRPC response = $rpc_response"
 }
